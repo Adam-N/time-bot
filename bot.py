6343206cc5d2099691821e8e0225b06503c1f8d5
Adam <32937074+Adam-N@users.noreply.github.com>
2019-03-27 17:31:40 -0600

    Update bot.py
    Added error catching in command.

--- bot.py
+++ bot.py
@@ -25,7 +25,6 @@ async def time(ctx):
     timezones_to_convert_dict = {
         'PT/California': 'America/Los_Angeles',
         'MT/Alberta': 'America/Edmonton',
-        'SK/Saskatchewan': 'Canada/Saskatchewan',
         'CT/Winnipeg': 'America/Winnipeg',
         'ET/New York': 'America/New_York',
         'UK/London': 'Europe/London',
@@ -49,48 +48,48 @@ async def time(ctx):
 async def convert(ctx, time_zone_input: str, *time_input: str):
     """Shows a range of timezones in the future. Format: h:m day/month/year. Example 12:30 4/8/2018"""
     await bot.send_typing(ctx.message.channel)
-    time_join = " ".join(time_input)
-    time_join.strip(' ')
-    time_to_convert = datetime.datetime.strptime(str(time_join), '%H:%M %d/%m/%y')
+    try:
+        time_join = " ".join(time_input)
+        time_join.strip(' ')
+        time_to_convert = datetime.datetime.strptime(str(time_join), '%H:%M %d/%m/%y')
 
-    tz_dict = {
-        'PT': 'America/Los_Angeles',
-        'MT': 'America/Edmonton',
-        'CT': 'America/Winnipeg',
-        'ET': 'America/New_York',
-        'SK': 'Canada/Saskatchewan',
-        'UK': 'Europe/London',
-        'CET': 'Europe/Paris',
-        'AU': 'Australia/Sydney'
-    }
-    timezones_to_use = {
-        'PT/California': 'America/Los_Angeles',
-        'MT/Alberta': 'America/Edmonton',
-        'SK/Saskatchewan': 'Canada/Saskatchewan',
-        'CT/Winnipeg': 'America/Winnipeg',
-        'ET/New York': 'America/New_York',
-        'UK/London': 'Europe/London',
-        'CET/Copenhagen': 'Europe/Copenhagen',
-        'AEST/Sydney': 'Australia/Sydney',
-    }
-    if time_zone_input in tz_dict.keys():
+        tz_dict = {
+            'PT': 'America/Los_Angeles',
+            'MT': 'America/Edmonton',
+            'CT': 'America/Winnipeg',
+            'ET': 'America/New_York',
+            'SK': 'Canada/Saskatchewan',
+            'UK': 'Europe/London',
+            'CET': 'Europe/Paris',
+            'AU': 'Australia/Sydney'
+        }
+        timezones_to_use = {
+            'PT/California': 'America/Los_Angeles',
+            'MT/Alberta': 'America/Edmonton',
+            'SK/Saskatchewan': 'Canada/Saskatchewan',
+            'CT/Winnipeg': 'America/Winnipeg',
+            'ET/New York': 'America/New_York',
+            'UK/London': 'Europe/London',
+            'CET/Copenhagen': 'Europe/Copenhagen',
+            'AEST/Sydney': 'Australia/Sydney',
+        }
+
         from_zone = tz.gettz(tz_dict.get(time_zone_input))
-    else:
-        await bot.say('Please use an acceptable timezone. Use command ?timezones to see available timezones.')
-        return None
-    tz_field = []
-    time_field = []
-    for display, zone in timezones_to_use.items():
-        tz_field.append("**{}**".format(display))
-        mt = time_to_convert.replace(tzinfo=from_zone)
-        to_zone = tz.gettz(zone)
-        converted = mt.astimezone(to_zone)
-        time_field.append(converted.strftime('%H:%M %d/%m/%y'))
+        tz_field = []
+        time_field = []
+        for display, zone in timezones_to_use.items():
+            tz_field.append("**{}**".format(display))
+            mt = time_to_convert.replace(tzinfo=from_zone)
+            to_zone = tz.gettz(zone)
+            converted = mt.astimezone(to_zone)
+            time_field.append(converted.strftime('%H:%M %d/%m/%y'))
 
-    embed = discord.Embed(title="Time Conversions", description='In the Future', color=0x00ff00)
-    embed.add_field(name="Time Zones", value='\n'.join(tz_field), inline=True)
-    embed.add_field(name="Time", value='\n'.join(time_field), inline=True)
-    await bot.say(embed=embed)
+        embed = discord.Embed(title="Time Conversions", description='In the Future', color=0x00ff00)
+        embed.add_field(name="Time Zones", value='\n'.join(tz_field), inline=True)
+        embed.add_field(name="Time", value='\n'.join(time_field), inline=True)
+        await bot.say(embed=embed)
+    except ValueError:
+        await bot.say('Check formatting for your command. Must be in HH:MM d/m/y using the 24 hour clock.')
 
 
 @bot.command()
